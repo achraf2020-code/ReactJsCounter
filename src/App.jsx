fcd5b3de45935ee9e067008686de87d91699d93c
achraf2020-code <achraf-saidi@outlook.com>
2022-08-02 03:24:14 +0200

    save changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Counter from "./components/Counter"
 const App = ()=>{
   return(
     <div className="container">
-        <h1 className="Componnent--Title">ReactJs Counter Component count <strong>from 1 to 100</strong> </h1>
+        <div className="Componnent--Title">ReactJs Counter Component count <strong>from 1 to 100</strong> </div>
         <Counter/>
     </div>
   )
